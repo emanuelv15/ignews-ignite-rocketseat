fcf97c6bbee7500348448b14de06ff53f5489afb
Emanuel Vitor <emanuel.v.souza@hotmail.com>
2021-05-05 14:50:31 -0300

    prismic CMS configured

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,43 +1,52 @@
-import { useSession, signIn } from 'next-auth/client'
+import { useSession, signIn } from 'next-auth/client';
+import { useRouter } from 'next/router';
+import { Session } from 'next-auth/index';
 
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 
-import styles from './styles.module.scss'
+import styles from './styles.module.scss';
 
+interface SessionProps extends Session {
+  activeSubscription?: null | object;
+}
 interface SubscribeButtonProps {
   priceId: string;
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
+  const router = useRouter();
+
+  const sessionAux: SessionProps = session;
 
   async function handleSubscribe() {
     if (!session) {
-      signIn('github')
+      signIn('github');
+      return;
+    }
+
+    if (sessionAux?.activeSubscription) {
+      router.push('/posts');
       return;
     }
 
     try {
-      const response = await api.post('/subscribe')
+      const response = await api.post('/subscribe');
 
-      const { sessionId } = response.data
+      const { sessionId } = response.data;
 
-      const stripe = await getStripeJs()
+      const stripe = await getStripeJs();
 
-      await stripe.redirectToCheckout({ sessionId })
+      await stripe.redirectToCheckout({ sessionId });
     } catch (err) {
-      alert(err.message)
+      alert(err.message);
     }
   }
 
   return (
-    <button
-      type="button"
-      className={styles.subscribeButton}
-      onClick={handleSubscribe}
-    >
+    <button type="button" className={styles.subscribeButton} onClick={handleSubscribe}>
       Subscribe now
     </button>
-  )
+  );
 }
